ca333ba8fb88656fc3ccbc87b32af6b863f0c8c6
AyimWilliam <odhisayim@gmail.com>
2023-04-08 11:38:29 +0300

    Almost done

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # FIND-A-FARMER
 
 #### {Brief description of application}, {Date of current version}
-This mvp showcases Find a farmer that allows farmers to find ready market for their products. Users can sell their goods from anywhere across the country. The application offers a range of features such as an insurance offer and a gallery where customers can view showcased products from our farmers.(version 04/06/2023) 
+
+This mvp showcases Find a farmer that allows farmers to find ready market for their products. Users can sell their goods from anywhere across the country. The application offers a range of features such as an insurance offer and a gallery where customers can view showcased products from our farmers.(version 04/06/2023)
+
 #### By **{List of contributors}**
+
 This project was created and is sole property of Ayim William
+
 ## Description
-This is a single paged platform that allows users to buy and sell different agricultural products that meet their desires and at affordable prices. We  are basically connecting the farmer to wholesale and retail customers across Kenya and eliminating the menace of the broker who buys the goods cheaply and sells them expensively reducing the farmers profits by a lot. Users can see which items are in stock and where they can be found and only allows the user to buy when there is stock of the product. The user can also get updates on Find a Farmer news and can get access to our exclusive Multi-Layered Insurance for the farmers. The farmer can also delete a product that displeases him or is out of stock. The buy button is only active when there is available stock and will disable and change the text once stock is finished. The add button will be added later in the larger project.
+
+This is a single paged platform that allows users to buy and sell different agricultural products that meet their desires and at affordable prices. We are basically connecting the farmer to wholesale and retail customers across Kenya and eliminating the menace of the broker who buys the goods cheaply and sells them expensively reducing the farmers profits by a lot. Users can see which items are in stock and where they can be found and only allows the user to buy when there is stock of the product. The user can also get updates on Find a Farmer news and can get access to our exclusive Multi-Layered Insurance for the farmers. The farmer can also delete a product that displeases him or is out of stock. The buy button is only active when there is available stock and will disable and change the text once stock is finished. The add button will be added later in the larger project.
 
 ## Demo
+
 Use the link provided to navigate to the Find A Farmer website
 
 The page appears as shown below
 
 This is the landing page
 
-
 ## Setup/Installation Requirements IF yu need to contribute to the project
-* One would need either linux or wsl for window users
-* A copy of visual basic code installed
-* A github account
+
+- One would need either linux or wsl for window users
+- A copy of visual basic code installed
+- A github account
 
 1. Open your terminal and go to the directory you wish to work from.
 2. Go to the following url using ur github account https://github.com/BabaMboga/Week-3-Code-Challenge
@@ -30,10 +36,16 @@ This is the landing page
 8. Add the changes (git add .)
 9. Commit changes (git commit -m "contributing changes")
 10. Push your changes (git push origin <branchname>)
+
 ## Technologies Used
+
 This program is built with html, css and javascript using the visual code environment.
 It also has db.json file which contains the data we are going to use to populate the cards.
+
 ## Support and contact details
+
 For any issues please email me at william.ayim@student.moringaschool.com
+
 ### License
+
 Apache License 2.0
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,225 +1,249 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("loginForm").reset();
+  const galleryContainer = document.querySelector(".card-container");
 
-  document.getElementById('loginForm').reset();
-    const galleryContainer = document.querySelector('.card-container');
-  
-    // Make a request to the API
-    fetch('http://localhost:3000/products')
-      .then(response => response.json())
-      .then(products => {
-        // Loop through the data and create a card for each item
-        products.forEach((product) => {
-          const card = document.createElement('div');
-          card.classList.add('card');
-  
-          const image = document.createElement('img');
-          image.src = product.productimage;
-          card.appendChild(image);
-  
-          const title = document.createElement('h3');
-          title.textContent = `Name: ${product.productname}`;
-          card.appendChild(title);
-  
-          const location = document.createElement('p');
-          location.textContent = product.productlocation;
-          card.appendChild(location);
-  
-          const stock = document.createElement('p');
-          stock.textContent = `Available Stock: ${product.itemstock}`;
-          card.appendChild(stock);
+  // Make a request to the API
+  fetch("http://localhost:3000/products")
+    .then((response) => response.json())
+    .then((products) => {
+      // Loop through the data and create a card for each item
+      products.forEach((product) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-          const retailPrice = document.createElement('p');
-          retailPrice.textContent = `Retail Price: ${product.itemprice}`;
-          card.appendChild(retailPrice);
+        const image = document.createElement("img");
+        image.src = product.productimage;
+        card.appendChild(image);
 
-          const wholesalePrice = document.createElement('p');
-          wholesalePrice.textContent = `Wholesale Price: ${product.wholesaleprice}`;
-          card.appendChild(wholesalePrice);
-  
-          const buyButton = document.createElement('button');
-          buyButton.className = 'btn' ;
-          buyButton.textContent = 'Buy';
-          buyButton.addEventListener('click', () => {
-            // Prompt the user for the amount of stock to be purchased
-            const purchasedAmount = prompt('Enter the amount of stock to be purchased:');
-            if (purchasedAmount !== null && purchasedAmount !== '' && !isNaN(purchasedAmount) && parseInt(purchasedAmount) > 0) {
-              const newStock = product.itemstock - purchasedAmount;
-              // Make a request to update the API with the amount of stock purchased
-              fetch(`http://localhost:3000/products/${product.id}`, {
-                method: 'PATCH',
-                body: JSON.stringify({ itemstock: newStock }),
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              })
-                .then(response => response.json())
-                .then(updatedItem => {
-                  // Update the card with the new stock information
-                  stock.textContent = `Available Stock: ${updatedItem.itemstock}`;
-                  if (updatedItem.itemstock === 0) {
-                    buyButton.textContent = 'Sold Out';
-                    buyButton.disabled = true;
-                  }
-                })
-                .catch(error => {
-                  console.error(error);
-                  alert('Failed to update the amount of stock purchased.');
-                });
-              if (product.itemstock === 0) {
-                buyButton.textContent = 'Sold Out';
-                buyButton.disabled = true;
-              }
-            }
-          });
-  
-          const deleteButton = document.createElement('button');
-          deleteButton.className = 'btn';
-          deleteButton.textContent = 'Delete';
-          deleteButton.addEventListener('click', () => {
-            // Make a request to delete the product from the API
+        const title = document.createElement("h3");
+        title.textContent = `Name: ${product.productname}`;
+        card.appendChild(title);
+
+        const location = document.createElement("p");
+        location.textContent = product.productlocation;
+        card.appendChild(location);
+
+        const stock = document.createElement("p");
+        stock.textContent = `Available Stock: ${product.itemstock}`;
+        card.appendChild(stock);
+
+        const retailPrice = document.createElement("p");
+        retailPrice.textContent = `Retail Price: ${product.itemprice}`;
+        card.appendChild(retailPrice);
+
+        const wholesalePrice = document.createElement("p");
+        wholesalePrice.textContent = `Wholesale Price: ${product.wholesaleprice}`;
+        card.appendChild(wholesalePrice);
+
+        const buyButton = document.createElement("button");
+        buyButton.className = "btn";
+        buyButton.textContent = "Buy";
+        buyButton.addEventListener("click", () => {
+          // Prompt the user for the amount of stock to be purchased
+          const purchasedAmount = prompt(
+            "Enter the amount of stock to be purchased:"
+          );
+          if (
+            purchasedAmount !== null &&
+            purchasedAmount !== "" &&
+            !isNaN(purchasedAmount) &&
+            parseInt(purchasedAmount) > 0
+          ) {
+            const newStock = product.itemstock - purchasedAmount;
+            // Make a request to update the API with the amount of stock purchased
             fetch(`http://localhost:3000/products/${product.id}`, {
-              method: 'DELETE',
+              method: "PATCH",
+              body: JSON.stringify({ itemstock: newStock }),
+              headers: {
+                "Content-Type": "application/json",
+              },
             })
-              .then(response => {
-                if (!response.ok) {
-                  throw new Error('Failed to delete product');
+              .then((response) => response.json())
+              .then((updatedItem) => {
+                // Update the card with the new stock information
+                stock.textContent = `Available Stock: ${updatedItem.itemstock}`;
+                if (updatedItem.itemstock === 0) {
+                  buyButton.textContent = "Sold Out";
+                  buyButton.disabled = true;
                 }
-                // Remove the card from the gallery container
-                galleryContainer.removeChild(card);
               })
-              .catch(error => {
+              .catch((error) => {
                 console.error(error);
-                alert('Failed to delete product from the API.');
+                alert("Failed to update the amount of stock purchased.");
               });
-          });
-  
-          card.appendChild(buyButton);
-          card.appendChild(deleteButton);
-          galleryContainer.appendChild(card);
-        });
-      })
-      .catch(error => {
-        console.error(error);
-        alert('Failed to fetch products from the API.');
-      });
-
-      const addItemsButton = document.getElementById('addItems');
-      addItemsButton.addEventListener('click', () => {
-        //promp the user to enter the details of the new item
-        const newProduct = {};
-        newProduct.productname = prompt('Enter the product name:');
-        newProduct.productimage = prompt('Enter the product image URL:');
-        newProduct.productlocation = prompt('Enter the product location:');
-        newProduct.itemstock = prompt('Enter the product stock:');
-        newProduct.Firstname = prompt("Enter the owner's First Name:");
-        newProduct.Secondname = prompt("Enter the owner's Second Name:");
-        newProduct.farmerContactnumber = prompt("Enter the owner's Contact Number:");
-        newProduct.itemprice = prompt("Enter the product's retail price:");
-        newProduct.wholesaleprice = prompt("Enter the product's wholesale price:");
-
-        if(newProduct.productname && newProduct.productimage && newProduct.productlocation && newProduct.itemstock && newProduct.Firstname && newProduct.Secondname && newProduct.farmerContactnumber && newProduct.itemprice && newProduct.wholesaleprice){
-          fetch('http://localhost:3000/products', {
-            method: 'POST',
-            body: JSON.stringify(newProduct),
-            headers: {
-              'Content-Type': 'application/json'
+            if (product.itemstock === 0) {
+              buyButton.textContent = "Sold Out";
+              buyButton.disabled = true;
             }
+          }
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "btn";
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => {
+          // Make a request to delete the product from the API
+          fetch(`http://localhost:3000/products/${product.id}`, {
+            method: "DELETE",
           })
-          .then(response => response.json())
-          .then(newItem => {
-            // create a card for the new item and add it to the gallery
-            const newCard = document.createElement('div');
-            newCard.ckassKist.add('card');
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("Failed to delete product");
+              }
+              // Remove the card from the gallery container
+              galleryContainer.removeChild(card);
+            })
+            .catch((error) => {
+              console.error(error);
+              alert("Failed to delete product from the API.");
+            });
+        });
 
-            const newImage = document.createElement('img');
-            newImage.src = newItem.productimage;
-            newCard.appendChild(newImage);
+        card.appendChild(buyButton);
+        card.appendChild(deleteButton);
+        galleryContainer.appendChild(card);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Failed to fetch products from the API.");
+    });
 
-            const newTitle = document.createElement('h3');
-            newTitle.textContent = newItem.productname;
-            newCard.appendChild(newTitle);
+  const addItemsButton = document.getElementById("addItems");
+  addItemsButton.addEventListener("click", () => {
+    //promp the user to enter the details of the new item
+    const newProduct = {};
+    newProduct.productname = prompt("Enter the product name:");
+    newProduct.productimage = prompt("Enter the product image URL:");
+    newProduct.productlocation = prompt("Enter the product location:");
+    newProduct.itemstock = prompt("Enter the product stock:");
+    newProduct.Firstname = prompt("Enter the owner's First Name:");
+    newProduct.Secondname = prompt("Enter the owner's Second Name:");
+    newProduct.farmerContactnumber = prompt(
+      "Enter the owner's Contact Number:"
+    );
+    newProduct.itemprice = prompt("Enter the product's retail price:");
+    newProduct.wholesaleprice = prompt("Enter the product's wholesale price:");
 
-            const newLocation = document.createElement('p');
-            newLocation.textContent = newItem.productlocation;
-            card.appendChild(newLocation);
-  
-            const newStock = document.createElement('p');
-            newStock.textContent = `Available Stock: ${newItem.itemstock}`;
-            card.appendChild(newStock);
+    if (
+      newProduct.productname &&
+      newProduct.productimage &&
+      newProduct.productlocation &&
+      newProduct.itemstock &&
+      newProduct.Firstname &&
+      newProduct.Secondname &&
+      newProduct.farmerContactnumber &&
+      newProduct.itemprice &&
+      newProduct.wholesaleprice
+    ) {
+      fetch("http://localhost:3000/products", {
+        method: "POST",
+        body: JSON.stringify(newProduct),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((newItem) => {
+          // create a card for the new item and add it to the gallery
+          const newCard = document.createElement("div");
+          newCard.ckassKist.add("card");
 
-            const newRetailPrice = document.createElement('p');
-            newRetailPrice.textContent = `Retail Price: ${newItem.itemprice}`;
-            card.appendChild(newRetailPrice);
+          const newImage = document.createElement("img");
+          newImage.src = newItem.productimage;
+          newCard.appendChild(newImage);
 
-            const newWholesalePrice = document.createElement('p');
-            newWholesalePrice.textContent = `Wholesale Price: ${newItem.wholesaleprice}`;
-            card.appendChild(newWholesalePrice);
-  
-            const newBuyButton = document.createElement('button');
-            newBuyButton.className = 'btn' ;
-            newBuyButton.textContent = 'Buy';
-            newBuyButton.addEventListener('click', () => {
-              //prompt the user for the amount of stock to be purchsded 
-              const purchasedAmount = prompt ('Enter the emount of stock to be purchased:');
+          const newTitle = document.createElement("h3");
+          newTitle.textContent = newItem.productname;
+          newCard.appendChild(newTitle);
 
-              if (purchasedAmount !== null && purchasedAmount !== '' && !isNaN(purchasedAmount) && parseInt(purchasedAmount) > 0) {
-                const newStockAmount = newItem.itemstock -  purchasedAmount;
-                //Make a request to update the API with the amount of stock purchased
-                fetch(`http://localhost:3000/products/${newItem.id}`,{
-                  method: 'PATCH',
-                  body: JSON.stringify({itemstock : newStockAmount}),
-                  headers: {
-                    'Content-Type' : 'application/json'
-                  }
-                })
-                .then(response => response.json())
-                .then(updatedItem => {
+          const newLocation = document.createElement("p");
+          newLocation.textContent = newItem.productlocation;
+          card.appendChild(newLocation);
+
+          const newStock = document.createElement("p");
+          newStock.textContent = `Available Stock: ${newItem.itemstock}`;
+          card.appendChild(newStock);
+
+          const newRetailPrice = document.createElement("p");
+          newRetailPrice.textContent = `Retail Price: ${newItem.itemprice}`;
+          card.appendChild(newRetailPrice);
+
+          const newWholesalePrice = document.createElement("p");
+          newWholesalePrice.textContent = `Wholesale Price: ${newItem.wholesaleprice}`;
+          card.appendChild(newWholesalePrice);
+
+          const newBuyButton = document.createElement("button");
+          newBuyButton.className = "btn";
+          newBuyButton.textContent = "Buy";
+          newBuyButton.addEventListener("click", () => {
+            //prompt the user for the amount of stock to be purchsded
+            const purchasedAmount = prompt(
+              "Enter the emount of stock to be purchased:"
+            );
+
+            if (
+              purchasedAmount !== null &&
+              purchasedAmount !== "" &&
+              !isNaN(purchasedAmount) &&
+              parseInt(purchasedAmount) > 0
+            ) {
+              const newStockAmount = newItem.itemstock - purchasedAmount;
+              //Make a request to update the API with the amount of stock purchased
+              fetch(`http://localhost:3000/products/${newItem.id}`, {
+                method: "PATCH",
+                body: JSON.stringify({ itemstock: newStockAmount }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              })
+                .then((response) => response.json())
+                .then((updatedItem) => {
                   //Update the card with the new stock infomation
                   newStock.textContent = `${updatedItem.itemstock}`;
-                  if(updatedItem.itemstock === 0 ) {
-                    newBuyButton.textContent = 'Sold Out';
+                  if (updatedItem.itemstock === 0) {
+                    newBuyButton.textContent = "Sold Out";
                     newBuyButton.disabled = true;
                   }
                 })
-                .catch(error => {
+                .catch((error) => {
                   console.error(error);
-                  alert('Failed to update the amount of stock purchased.');
+                  alert("Failed to update the amount of stock purchased.");
                 });
-                if(newItem.itemstock === 0){
-                  newBuyButton.textContent = 'Sold Out';
-                  newBuyButton.disabled = true;
-                }
+              if (newItem.itemstock === 0) {
+                newBuyButton.textContent = "Sold Out";
+                newBuyButton.disabled = true;
               }
-            });
-            newCard.appendChild(newBuyButton);
+            }
+          });
+          newCard.appendChild(newBuyButton);
 
-            const newDeleteButton = document.createElement('button');
-            newDeleteButton.textContent = 'Delete';
-            newDeleteButton.addEventListener('click', () => {
-              // Make a request to delete the item from the API
-              fetch(`http://localhost:3000/products/${newItem.id}`,{
-                method: 'DELETE'
-              })
+          const newDeleteButton = document.createElement("button");
+          newDeleteButton.textContent = "Delete";
+          newDeleteButton.addEventListener("click", () => {
+            // Make a request to delete the item from the API
+            fetch(`http://localhost:3000/products/${newItem.id}`, {
+              method: "DELETE",
+            })
               .then(() => {
                 //remove the card from the gallery
                 newCard.remove();
               })
-              .catch(error => {
+              .catch((error) => {
                 console.error(error);
-                alert('Failed to delete the item.')
+                alert("Failed to delete the item.");
               });
-            });
-            newCard.appendChild(newDeleteButton);
-
-            galleryContainer.appendChild(newCard);
-          })
-          .catch(error => {
-            console.error(error);
-            alert('Failed to add the item to the API.');
           });
-        } else {
-          alert('Please enter all details to add a new item.');
-        }
-      });
+          newCard.appendChild(newDeleteButton);
+
+          galleryContainer.appendChild(newCard);
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Failed to add the item to the API.");
+        });
+    } else {
+      alert("Please enter all details to add a new item.");
+    }
+  });
 });
-  
