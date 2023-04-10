096aea0e0672b6ba2deeb4719be48cbf531923e1
AyimWilliam <odhisayim@gmail.com>
2023-04-10 20:31:16 +0300

    Last trial link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+ });
+ 
   document.getElementById("loginForm").reset();
   const galleryContainer = document.querySelector(".card-container");
 
