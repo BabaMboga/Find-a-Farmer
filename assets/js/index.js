794cf7d6bb7c840dc957df3572455fcc274ddcb2
AyimWilliam <odhisayim@gmail.com>
2023-04-10 18:23:20 +0300

    new db.json link

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const galleryContainer = document.querySelector(".card-container");
 
   // Make a request to the API
-  fetch("db.json")
+  fetch("https://github.com/BabaMboga/Find-a-Farmer/blob/main/db.json")
     .then((response) => response.json())
     .then((products) => {
       // Loop through the data and create a card for each item
