237dce3bef5ba7dc6d5b0f437f616decb11a8933
AyimWilliam <odhisayim@gmail.com>
2023-04-10 20:19:11 +0300

    Last trial link

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const galleryContainer = document.querySelector(".card-container");
 
   // Make a request to the API
-  fetch(" https://my-json-server.typicode.com/BabaMboga/Find-a-Farmer/ ")
+  fetch("  https://my-json-server.typicode.com/Find-a-Farmer/BabaMboga  ")
     .then((response) => response.json())
     .then((products) => {
       // Loop through the data and create a card for each item
